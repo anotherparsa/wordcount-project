bdccf979d33c9cdc8be686e6d2988657810e334e
another parsa <anotherparsa@gmail.com>
2022-06-14 11:39:12 +0430

    fixed a little bug

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ def result():
     wordNumber = len(wordCounter.keys())
     return render_template("result/result.html", wordNumber=wordNumber, wordCounter=wordCounter, words=list(set(words)))
 
+    
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
